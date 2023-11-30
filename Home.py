a01497ace84d1a21fe6ddf09a2a9a78e6ea90964
panda <cuong.huynh@vota.vn>
2023-11-30 14:05:32 +0700

    update

--- Home.py
+++ Home.py
@@ -9,7 +9,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 from PIL import Image
 
-st.set_page_config(layout="wide")
+# st.set_page_config(layout="wide")
 
 st.header("Analytic Geometry")
 
